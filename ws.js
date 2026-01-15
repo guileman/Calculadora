f2ee1eba72989334787dd05a6264899ab376948b
Guillermo Diaz <guileman.diaz@gmail.com>
2026-01-14 23:21:54 -0300

    Add files via upload

--- ws.js
+++ ws.js
@@ -1,7 +1,29 @@
+const CACHE = 'calc-losa-v3';
+
+const FILES = [
+  '/Calculadora/',
+  '/Calculadora/index.html',
+  '/Calculadora/manifest.json'
+];
+
 self.addEventListener('install', e => {
   e.waitUntil(
-    caches.open('losa-cache').then(cache =>
-      cache.addAll(['index.html'])
+    caches.open(CACHE).then(c => c.addAll(FILES))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
     )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
   );
 });
